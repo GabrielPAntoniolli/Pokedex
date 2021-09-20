6c86756643b772f107da38cd2b34cdf19437afa4
GabrielPAntoniolli <72032836+GabrielPAntoniolli@users.noreply.github.com>
2021-09-20 10:26:42 +0100

    fetching from the database now with criteria API

--- src/main/java/com/JPAProject/Pokedex/Service/PokemonService.java
+++ src/main/java/com/JPAProject/Pokedex/Service/PokemonService.java
@@ -2,17 +2,11 @@ package com.JPAProject.Pokedex.Service;
 
 import com.JPAProject.Pokedex.DTO.PokemonDTO;
 import com.JPAProject.Pokedex.Dao.PokemonDAO_WithCriteriaBuilder;
-import com.JPAProject.Pokedex.Dao.PokemonRepository;
 import com.JPAProject.Pokedex.Entity.Pokemon;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,12 +18,6 @@ public class PokemonService {
 
     @Autowired
     PokemonDAO_WithCriteriaBuilder pokeDAO;
-
-    /*public Page<PokemonDTO> getAllPokemon(Pageable pageable) {
-
-        Page<PokemonDTO> dtoPage = pokemonRepository.findAll(pageable).map(pokemon -> modelMapper.map(pokemon,PokemonDTO.class));
-        return dtoPage;
-    }*/
 
     public List<PokemonDTO> findAll(){
 
